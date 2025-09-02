413b020d3573512719727d78c3c8f84d34b05b44
a-hazratov <artur.hazratov@gmail.com>
2025-09-02 08:21:10 +0200

    added required files and definitions

--- service.py
+++ service.py
@@ -3,6 +3,19 @@ from pydantic import BaseModel
 import asyncio
 import os
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Expected format for requests___________________________
 class GlobalInput(BaseModel):
@@ -20,17 +33,31 @@ class GlobalInput(BaseModel):
     #anomaly_flag                :
 #___________________________
 
+data = pd.DataFrame()
+csv_path = None
+
+
+@app.post("/upload_csv/")
+async def upload_csv(file: UploadFile = File(...)):
+    global csv_path, data
+    csv_path = f"./dataset_file.csv"  # save file to local dir and make sure that only obe file is there at a time
+    with open(csv_path, "wb") as f:
+        f.write(await file.read())
+
+    data = pd.read_csv(csv_path)  # data is now the uploaded csv
+    return {"status": "success", "message": f"Your csv has been uploaded, and saved to {csv_path}"}
+
 #To set a csv as data___________________
 def set_csv(csv_name: str):
     global data, file_path
     file_path = fr".\{csv_name}"
 
     if os.path.exists:
-        global data = pd.read_csv(file_path)
+        global data
+        data = pd.read_csv(file_path)
         return f"{csv_name} set as source data."
     else:
-        return f"No file labelled {csv_name} found on the server. 
-                 Please check the filename."        
+        return f"No file labelled {csv_name} found on the server. Please check the filename."
     
 
 #Train the model, IF needed___________________
